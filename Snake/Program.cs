94794b8ea905a6aeddabb65a23f53fc6fad33e57
WKE\francisco.lopez <francisco.lopez@external.wolterskluwer.com>
2023-03-22 15:32:31 +0100

    upgrade to .NET 7.0

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,6 +14,7 @@ namespace Snake
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SnakeMainForm());
         }
